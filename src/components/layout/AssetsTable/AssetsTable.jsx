4c1488e3ccf4b9c63d7408f51f6ed2d9cbf4604c
Ivan <ivanitit3535@gmail.com>
2026-03-12 19:21:11 +0200

    refactor: Assetetable

--- src/components/layout/AssetsTable/AssetsTable.jsx
+++ src/components/layout/AssetsTable/AssetsTable.jsx
@@ -1,34 +1,41 @@
 import { useCrypto } from '@/context/cryptoContext';
 import { Table } from 'antd';
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    sorter: (a, b) => a.name.length - b.name.length,
-    sortDirections: ['descend'],
-  },
-  {
-    title: 'Price $',
-    dataIndex: 'price',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.price - b.price,
-  },
-  {
-    title: 'Amount',
-    dataIndex: 'amount',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.amount - b.amount,
-  },
-];
 
 export default function AssetsTable() {
   const { assets } = useCrypto();
 
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      sorter: (a, b) => a.name.localeCompare(b.name),
+      sortDirections: ['ascend', 'descend'],
+    },
+    {
+      title: 'Price $',
+      dataIndex: 'price',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.price - b.price,
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.amount - b.amount,
+    },
+    {
+      title: 'Total $',
+      dataIndex: 'totalValue',
+      sorter: (a, b) => a.totalValue - b.totalValue,
+    },
+  ];
+
   const data = assets.map((a) => ({
     key: a.id,
     name: a.name,
-    price: a.price,
-    amount: a.amount,
+    price: a.price ?? 0,
+    amount: a.amount ?? 0,
+    totalValue: +(a.price ?? 0) * (a.amount ?? 0),
   }));
 
   return (
@@ -36,7 +43,7 @@ export default function AssetsTable() {
       pagination={false}
       columns={columns}
       dataSource={data}
-      showSorterTooltip={{ target: 'sorter-icon' }}
+      showSorterTooltip={{ title: 'Click to sort' }}
     />
   );
 }
