2395557b01e85cfccf9974ab2fb9a361465ed227
Ivan <ivanitit3535@gmail.com>
2026-03-12 19:28:37 +0200

    refactor: CoinInfo

--- src/components/layout/CoinInfo/CoinInfo.jsx
+++ src/components/layout/CoinInfo/CoinInfo.jsx
@@ -1,12 +1,11 @@
-import { Flex, Typography } from 'antd';
-
 export default function CoinInfo({ coin, withSymbol }) {
   return (
-    <Flex align="center" gap={10}>
-      <img src={coin.icon} alt={coin.name} width={40} />
-      <Typography.Title level={2} style={{ margin: 0 }}>
-        {withSymbol && <span>({coin.symbol})</span>} {coin.name}
-      </Typography.Title>
-    </Flex>
+    <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
+      <img src={coin?.icon} alt={coin?.name ?? ''} width={40} />
+      <h2 style={{ margin: 0 }}>
+        {withSymbol && coin?.symbol ? `(${coin.symbol}) ` : ''}
+        {coin?.name ?? 'Unknown'}
+      </h2>
+    </div>
   );
 }
