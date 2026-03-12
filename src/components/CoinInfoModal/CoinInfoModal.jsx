4bc298fede4451e059b0a1f2e41064a1ed127f1f
Ivan <ivanitit3535@gmail.com>
2026-03-12 18:45:26 +0200

    refactor: CoinInfoModal

--- src/components/CoinInfoModal/CoinInfoModal.jsx
+++ src/components/CoinInfoModal/CoinInfoModal.jsx
@@ -1,37 +1,46 @@
-import { Divider, Flex, Tag, Typography } from 'antd';
+import { Divider, Tag, Typography } from 'antd';
 import CoinInfo from '../layout/CoinInfo/CoinInfo';
+
+const CHANGE_METRICS = [
+  { label: '1 hour', key: 'priceChange1h' },
+  { label: '1 day', key: 'priceChange1d' },
+  { label: '1 week', key: 'priceChange1w' },
+];
 
 export default function CoinInfoModal({ coin }) {
   return (
     <>
       <CoinInfo coin={coin} withSymbol />
       <Divider />
+
       <Typography.Paragraph>
-        <Typography.Text strong>1 hour: </Typography.Text>
-        <Tag color={coin.priceChange1h > 0 ? 'green' : 'red'}>
-          {coin.priceChange1h}%
-        </Tag>
-        <Typography.Text strong>1 day: </Typography.Text>
-        <Tag color={coin.priceChange1d > 0 ? 'green' : 'red'}>
-          {coin.priceChange1d}%
-        </Tag>
-        <Typography.Text strong>1 week: </Typography.Text>
-        <Tag color={coin.priceChange1w > 0 ? 'green' : 'red'}>
-          {coin.priceChange1w}%
-        </Tag>
+        {CHANGE_METRICS.map(({ label, key }) => (
+          <span key={key} style={{ marginRight: 8 }}>
+            <Typography.Text strong>{label}: </Typography.Text>
+            <Tag color={coin[key] > 0 ? 'green' : 'red'}>{coin[key]}%</Tag>
+          </span>
+        ))}
       </Typography.Paragraph>
+
       <Typography.Paragraph>
         <Typography.Text strong>Price: </Typography.Text>
-        {coin.price.toFixed(2)}$
+        {coin.price.toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}
+        $
       </Typography.Paragraph>
+
       <Typography.Paragraph>
         <Typography.Text strong>Price BTC: </Typography.Text>
         {coin.priceBtc}
       </Typography.Paragraph>
+
       <Typography.Paragraph>
         <Typography.Text strong>Market Cap: </Typography.Text>
-        {coin.marketCap}$
+        {coin.marketCap.toLocaleString()}$
       </Typography.Paragraph>
+
       {coin.contractAddress && (
         <Typography.Paragraph>
           <Typography.Text strong>Contract Address: </Typography.Text>
